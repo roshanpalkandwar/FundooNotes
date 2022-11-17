920462b439bf1239c8577dce9caddddc6075d43a
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-11-17 11:44:48 +0530

    [Roshan]All Crud Operation With User

--- FundoNotes/src/routes/user.route.js
+++ FundoNotes/src/routes/user.route.js
@@ -9,6 +9,18 @@ const router = express.Router();
 //route to Registration  new user
 router.post('/register', newUserValidator, userController.RegisterNewUser);
 
+//route to get all users
+router.get('', userController.getAllUsers);
+
+//route to get a single user by their user id
+router.get('/:_id',  userController.getUser);
+
+//route to update a single user by their user id
+router.put('/:_id', userController.updateUser);
+
+//route to delete a single user by their user id
+router.delete('/:_id', userController.deleteUser);
+
 
 
 export default router;
--- FundoNotes/src/services/user.service.js
+++ FundoNotes/src/services/user.service.js
@@ -27,3 +27,35 @@ export const Registration = async (body) => {
   }
   
 };
+
+//get all users
+export const getAllUsers = async () => {
+  const data = await User.find();
+  return data;
+};
+
+//update single user
+export const updateUser = async (_id, body) => {
+  const data = await User.findByIdAndUpdate(
+    {
+      _id
+    },
+    body,
+    {
+      new: true
+    }
+  );
+  return data;
+};
+
+//delete single user
+export const deleteUser = async (id) => {
+  await User.findByIdAndDelete(id);
+  return '';
+};
+
+//get single user
+export const getUser = async (_id) => {
+  const data = await User.findById(_id);
+  return data;
+};
