0de6e2b3f0e62f4bd1c05a6021c9a2e03e41e828
roshanpalkandwar <roshanpalkandwar@gmail.com>
2022-11-30 21:53:50 +0530

    [Roshan] Added:Produce and consume queue massage with rabbitmq

--- FundoNotes/src/middlewares/auth.middleware.js
+++ FundoNotes/src/middlewares/auth.middleware.js
@@ -14,13 +14,14 @@ dotenv.config();
 export const userAuth = async (req, res, next) => {
   try {
     let bearerToken = req.header('Authorization');
+    console.log("bearer token before============>",bearerToken)
     if (!bearerToken)
       throw {
         code: HttpStatus.BAD_REQUEST,
         message: 'Authorization token is required'
       };
     bearerToken = bearerToken.split(' ')[1];
-    const user  = await jwt.verify(bearerToken, process.env.SECRET_KEY);
+    const user  = await jwt.verify(bearerToken, process.env.SECRET_KEY); 
     req.body.UserId=user.Username;
     next();
   } catch (error) {
---
+++ FundoNotes/src/utils/rabbitmq.js
@@ -0,0 +1,61 @@
+var amqp = require('amqplib/callback_api');
+
+
+const producer = () => {
+    amqp.connect('amqp://localhost', function (error0, connection) {
+        if (error0) {
+            throw error0;
+        }
+        connection.createChannel(function (error1, channel) {
+            if (error1) {
+                throw error1;
+            }
+
+            var queue = 'hello';
+            var msg = 'Hello World!';
+
+            channel.assertQueue(queue, {
+                durable: false
+            });
+            channel.sendToQueue(queue, Buffer.from(msg));
+
+            console.log(" [x] Sent %s", msg);
+        });
+        setTimeout(function () {
+            connection.close();
+            process.exit(0);
+        }, 500);
+    });
+
+}
+producer()
+
+const consumer = () => {
+
+    amqp.connect('amqp://localhost', function (error0, connection) {
+        if (error0) {
+            throw error0;
+        }
+        connection.createChannel(function (error1, channel) {
+            if (error1) {
+                throw error1;
+            }
+
+            var queue = 'hello';
+
+            channel.assertQueue(queue, {
+                durable: false
+            });
+
+            console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
+
+            channel.consume(queue, function (msg) {
+                console.log(" [x] Received %s", msg.content.toString());
+            }, {
+                noAck: true
+            });
+        });
+    });
+
+}
+consumer();
